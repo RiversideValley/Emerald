51f3b99c56830e7197efd8b23f640a8626e2b44c
アンドレックス <developerwow64@outlook.com>
2022-10-02 19:14:36 +0100

    Add support for new name

--- Emerald.WinUI/Emerald.WinUI/Views/HomePage.xaml.cs
+++ Emerald.WinUI/Emerald.WinUI/Views/HomePage.xaml.cs
@@ -151,7 +151,7 @@ namespace Emerald.WinUI.Views
 
         private void treeVer_ItemInvoked(TreeView sender, TreeViewItemInvokedEventArgs args)
         {
-            btnVersion.Content = ((Models.MinecraftVersion)args.InvokedItem).SubVersions.Count > 0 ? btnVersion.Content : ((Models.MinecraftVersion)args.InvokedItem);
+            VersionButton.Content = ((Models.MinecraftVersion)args.InvokedItem).SubVersions.Count > 0 ? VersionButton.Content : ((Models.MinecraftVersion)args.InvokedItem);
             if(((Models.MinecraftVersion)args.InvokedItem).SubVersions.Count == 0)
             {
                 paneVersions.IsPaneOpen = false;
