e7a2754712622a0b8f0d5c397d529f96c34e7773
Chaniru Rajapakse <chaniru2007@outlook.com>
2022-10-23 00:26:19 +0530

    added loging function

--- Emerald.App/Emerald.App/App.xaml.cs
+++ Emerald.App/Emerald.App/App.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.UI.Xaml;
 using Windows.UI;
-
+using Serilog;
+using Windows.Storage;
+using System.IO;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
@@ -32,9 +34,15 @@ namespace Emerald.WinUI
         /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
+            Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Verbose()
+    .WriteTo.File(Path.Combine(ApplicationData.Current.LocalFolder.Path, "logs.log"))
+    .WriteTo.Console()
+    .CreateLogger();
             MainWindow = new MainWindow();
             MainWindow.Activate();
             Helpers.WindowManager.IntializeWindow(MainWindow);
+            MainWindow.Closed += (_,_) => Log.CloseAndFlush();
         }
 
         public static Window MainWindow { get; private set; }
