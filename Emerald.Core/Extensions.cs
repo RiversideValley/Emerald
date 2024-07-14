e28d758904c8be97f68ca694f4e2c3e3b92bc17f
Lamparter <notlamparter@outlook.com>
2024-07-14 18:53:05 +0100

    Remove spelling mistakes in files that reference 'Extensions.cs' (#38)

--- Emerald.Core/Extensions.cs
+++ Emerald.Core/Extensions.cs
@@ -1,6 +1,6 @@
 ﻿namespace Emerald.Core
 {
-    public static class Extentions
+    public static class Extensions
     {
         public static string ToFullVersion(this ProjBobcat.Class.Model.Optifine.OptifineDownloadVersionModel x)
             => $"{x.McVersion}-Optifine_{x.Type}_{x.Patch}";
