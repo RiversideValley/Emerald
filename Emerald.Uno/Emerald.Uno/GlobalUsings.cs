d05fb35ad3678b38060373d7dc3dcf57c8e01f83
NoobNotFound <chanirurajapakse@gmail.com>
2024-07-17 20:48:20 +0530

    Add fluent themed blank Uno app (maccatalyst, desktop)

---
+++ Emerald.Uno/Emerald.Uno/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using System.Collections.Immutable;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using ApplicationExecutionState = Windows.ApplicationModel.Activation.ApplicationExecutionState;
---
+++ Emerald.Uno/Emerald.Uno/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Emerald.Uno;
+
+public sealed partial class MainPage : Page
+{
+    public MainPage()
+    {
+        this.InitializeComponent();
+    }
+}
