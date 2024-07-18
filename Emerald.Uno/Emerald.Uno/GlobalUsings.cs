0642c5a67c69cf11b4df1e46fda306a7cfed0b26
Lamparter <notlamparter@outlook.com>
2024-07-18 17:15:42 +0100

    Remove Uno Platform project
    I didn't really like it anyway

--- Emerald.Uno/Emerald.Uno/GlobalUsings.cs
+++
@@ -1,5 +0,0 @@
-﻿global using System.Collections.Immutable;
-global using Microsoft.Extensions.DependencyInjection;
-global using Microsoft.Extensions.Hosting;
-global using Microsoft.Extensions.Logging;
-global using ApplicationExecutionState = Windows.ApplicationModel.Activation.ApplicationExecutionState;
--- Emerald.Uno/Emerald.Uno/MainPage.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace Emerald.Uno;
-
-public sealed partial class MainPage : Page
-{
-    public MainPage()
-    {
-        this.InitializeComponent();
-    }
-}
