d01e3de93fa233e3160148dcd7560e5f67932e70
NoobNotFound <82730163+NoobNotFound@users.noreply.github.com>
2026-05-27 20:11:56 +0530

    Add Emerald macOS SwiftUI app
    Add initial Xcode project scaffold for the Emerald app targeting macOS using SwiftUI. Files added: project.pbxproj, workspace and user scheme/state, asset catalogs (AppIcon, AccentColor, Assets), and source files (EmeraldApp.swift and ContentView.swift) with a basic "Hello, world!" UI. Includes initial build configurations and product/bundle settings.

---
+++ Emerald.SwiftUI/Emerald/Emerald/EmeraldApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmeraldApp.swift
+//  Emerald
+//
+//  Created by Chaniru Rajapakse on 2026-05-24.
+//
+
+import SwiftUI
+
+@main
+struct EmeraldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
